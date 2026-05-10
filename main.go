bfc87b3241dbde24e8a5c11496f5c6c8f1d12562
MyCode <230429220+MyCode83@users.noreply.github.com>
2026-05-10 11:52:52 +0100

    feat: add cli config type

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 
 	// Godirb-lib
 	"godirb/internal/assemble"
-
+	"godirb/internal/cli"
 	"godirb/internal/confirmation"
 	"godirb/internal/duration"
 	"godirb/internal/wordlist"
@@ -68,37 +68,7 @@ var preUserAgents = []string{
 	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
 }
 
-// Flags
-var (
-	url        string
-	threads    int
-	ignoreCode []int
-	exts       []string
-	rawTimeout  string
-	rawDelay	string
-	userAgent  []string
-	// bool
-	noColor   bool
-	recursive bool
-	//forces
-	forceHead    bool
-	forceThreads bool
-	forceProxy   bool
 
-	proxy  string
-	method string
-	// Basic Auth
-	username string
-	password string
-	// Placeholder
-	placeholder string
-
-	header []string
-	// TLS
-	insecure bool
-
-	quiet bool
-)
 
 // others
 var (
@@ -132,32 +102,37 @@ func main() {
 		log.Println(runtime.NumGoroutine())
 		// log.Println(": context canceled")
 	}()
-	wd := wordlist.Wordlist{}
-	// pflags
-	pflag.StringVarP(&url, "url", "u", "", "Target URL (e.g. https://localhost)")
-	pflag.StringVarP(&wd.Wordlist, "wordlist", "w", "medium", "Path to the wordlist. Defaults to raft-medium-directories from SecLists")
-	pflag.IntVarP(&threads, "threads", "t", 15, "Number of threads(goroutines) to use. Default: 15")
-	pflag.IntSliceVarP(&ignoreCode, "ignore", "i", []int{404, 400, 405, 408}, "Comma-separated list of status codes to ignore")
-	pflag.StringSliceVarP(&exts, "ext", "x", nil, "")
-	pflag.StringVarP(&rawTimeout, "timeout", "T", "5s", "Request timeout in seconds. Default: 5")
-	pflag.StringVarP(&rawDelay, "delay", "d", "0", "Request timeout in miliseconds. Default: 0") // ! Current
-	pflag.StringSliceVarP(&userAgent, "user-agent", "a", preUserAgents, "Comma-separated list of User-Agents to rotate")
-	// bools
-	pflag.BoolVarP(&noColor, "no-color", "n", false, "Disable colored output") // no color
-	pflag.BoolVarP(&recursive, "recursive", "r", false, "Recursive mode")      // recursive
-	// forces
-	pflag.BoolVarP(&forceHead, "force-head", "", false, "Skip wd confirmation")
-	pflag.BoolVarP(&forceThreads, "force-threads", "", false, "Skip threads confirmation")
-	pflag.BoolVarP(&forceProxy, "force-proxy", "", false, "Skip proxy confirmation")
 
-	pflag.StringVarP(&proxy, "proxy", "p", "", "HTTP/S proxy (e.g. http://127.0.0.1:8080)")
-	pflag.StringVarP(&method, "method", "m", "GET", "HTTP method to use: GET, HEAD, SWITCH (rotate)")
-	pflag.StringVarP(&username, "user", "U", "", "Username for Basic Auth")
-	pflag.StringVarP(&password, "password", "P", "", "Password for Basic Auth")
-	pflag.StringVar(&placeholder, "placeholder", "FUZZ", "Fuzzing Placeholder")
-	pflag.StringSliceVarP(&header, "header", "H", nil, "Add Header")
-	pflag.BoolVarP(&insecure, "insecure", "k", false, "Skip tls validation")
-	pflag.BoolVarP(&quiet, "quiet", "q", false, "Luego")
+
+	wd := wordlist.Wordlist{}
+	cfg := cli.Config{}
+
+
+	// pflags
+	pflag.StringVarP(&cfg.URL, "cfg.URL", "u", "", "Target URL (e.g. https://localhost)")
+	pflag.StringVarP(&wd.Wordlist, "wordlist", "w", "medium", "Path to the wordlist. Defaults to raft-medium-directories from SecLists")
+	pflag.IntVarP(&cfg.Threads, "cfg.Threads", "t", 15, "Number of cfg.Threads(goroutines) to use. Default: 15")
+	pflag.IntSliceVarP(&cfg.IgnoreCode, "ignore", "i", []int{404, 400, 405, 408}, "Comma-separated list of status codes to ignore")
+	pflag.StringSliceVarP(&cfg.Exts, "ext", "x", nil, "")
+	pflag.StringVarP(&cfg.RawTimeout, "timeout", "T", "5s", "Request timeout in seconds. Default: 5")
+	pflag.StringVarP(&cfg.RawDelay, "delay", "d", "0", "Request timeout in miliseconds. Default: 0") // ! Current
+	pflag.StringSliceVarP(&cfg.UserAgent, "user-agent", "a", preUserAgents, "Comma-separated list of User-Agents to rotate")
+	// bools
+	pflag.BoolVarP(&cfg.NoColor, "no-color", "n", false, "Disable colored output") // no color
+	pflag.BoolVarP(&cfg.Recursive, "cfg.Recursive", "r", false, "Recursive mode")      // cfg.Recursive
+	// forces
+	pflag.BoolVarP(&cfg.ForceHead, "force-head", "", false, "Skip wd confirmation")
+	pflag.BoolVarP(&cfg.ForceThreads, "force-cfg.Threads", "", false, "Skip cfg.Threads confirmation")
+	pflag.BoolVarP(&cfg.ForceProxy, "force-proxy", "", false, "Skip Proxy confirmation")
+
+	pflag.StringVarP(&cfg.Proxy, "cfg.Proxy", "p", "", "HTTP/S cfg.Proxy (e.g. http://127.0.0.1:8080)")
+	pflag.StringVarP(&cfg.Method, "cfg.Method", "m", "GET", "HTTP cfg.Method to use: GET, HEAD, SWITCH (rotate)")
+	pflag.StringVarP(&cfg.Username, "user", "U", "", "Username for Basic Auth")
+	pflag.StringVarP(&cfg.Password, "cfg.Password", "P", "", "Password for Basic Auth")
+	pflag.StringVar(&cfg.Placeholder, "cfg.Placeholder", "FUZZ", "Fuzzing cfg.Placeholder")
+	pflag.StringSliceVarP(&cfg.Header, "cfg.Header", "H", nil, "Add cfg.Header")
+	pflag.BoolVarP(&cfg.Insecure, "cfg.Insecure", "k", false, "Skip tls validation")
+	pflag.BoolVarP(&cfg.Quiet, "cfg.Quiet", "q", false, "Luego")
 
 
 	pflag.Usage = func() {
@@ -165,43 +140,43 @@ func main() {
 	}
 	pflag.Parse()
 	
-	if !quiet {
+	if !cfg.Quiet {
 		fmt.Printf(banner)
 		log.Println("Godirb v", version)
 		fmt.Println("Author: MyCode83")
 
 	}
-	BaseURL := strings.TrimRight(url, "/")
-	if noColor || quiet{
+	BaseURL := strings.TrimRight(cfg.URL, "/")
+	if cfg.NoColor || cfg.Quiet{
 			useColors = false
 	}
-	timeout, timeoutErr := duration.ParseDuration(rawTimeout, "s")
-	delay, delayErr := duration.ParseDuration(rawDelay, "ms")
+	timeout, timeoutErr := duration.ParseDuration(cfg.RawTimeout, "s")
+	delay, delayErr := duration.ParseDuration(cfg.RawDelay, "ms")
 	// Delay error
 	if delayErr != nil {
-		fmt.Fprintf(os.Stderr, "[X] An error ocurred during parsing durantion.\nCheck if you spelled it correctly '%s'", rawDelay)
+		fmt.Fprintf(os.Stderr, "[X] An error ocurred during parsing durantion.\nCheck if you spelled it correctly '%s'", cfg.RawDelay)
 		os.Exit(1)		
 	}
 	// Timeout error
 	if timeoutErr != nil {
-		fmt.Fprintf(os.Stderr, "[X] An error ocurred during parsing durantion.\nCheck if you spelled it correctly '%s'", rawTimeout)
+		fmt.Fprintf(os.Stderr, "[X] An error ocurred during parsing durantion.\nCheck if you spelled it correctly '%s'", cfg.RawTimeout)
 		os.Exit(1)
 	}
-	if strings.TrimSpace(url) == "" {
-		fmt.Fprintln(os.Stderr, "[X] Error, missing '--url'(-u) flag")
+	if strings.TrimSpace(cfg.URL) == "" {
+		fmt.Fprintln(os.Stderr, "[X] Error, missing '--cfg.URL'(-u) flag")
 		fmt.Fprintln(os.Stderr, "Run godirb --help for usage")
 		os.Exit(2)
 	}
-	if threads >= 250 && !forceThreads {
+	if cfg.Threads >= 250 && !cfg.ForceThreads {
 
-		if !confirmation.ThreadsConfirmation(fmt.Sprintf("Are you shure you want to send %d threads", threads)) {
+		if !confirmation.ThreadsConfirmation(fmt.Sprintf("Are you shure you want to send %d cfg.Threads", cfg.Threads)) {
 			fmt.Println("Cancelled by the user")
 			os.Exit(0)
 		}
 
 	}
-	if threads <= 0 {
-		fmt.Fprintf(os.Stderr, "[X] Error, you can't send %d threads.\n", threads)
+	if cfg.Threads <= 0 {
+		fmt.Fprintf(os.Stderr, "[X] Error, you can't send %d cfg.Threads.\n", cfg.Threads)
 		os.Exit(2)
 	}
 	if !useColors {
@@ -211,16 +186,16 @@ func main() {
 		fmt.Printf("[X] Error: timeout must be greater than 0\n")
 		os.Exit(2)
 	}
-	if parse.ExtractPort(BaseURL) == placeholder{
+	if parse.ExtractPort(BaseURL) == cfg.Placeholder{
 		mode = core.ModePort
-	} else if strings.Contains(BaseURL, placeholder) {
+	} else if strings.Contains(BaseURL, cfg.Placeholder) {
 		mode = core.ModeFuzz
 	}
-	client = assemble.BuildProxyAndClient(proxy, timeout, insecure) // Fasthttp-Client
+	client = assemble.BuildProxyAndClient(cfg.Proxy, timeout, cfg.Insecure) // Fasthttp-Client
 	switch mode {
 	case core.ModeFuzz:
-		if !pflag.Lookup("placeholder").Changed {
-			exts = []string{}
+		if !pflag.Lookup("cfg.Placeholder").Changed {
+			cfg.Exts = []string{}
 		}
 	case core.ModePort:
 		if !pflag.Lookup("wordlist").Changed {
@@ -237,7 +212,7 @@ func main() {
 			fmt.Printf("[!] Very high timeout (%s). Scan will be very slow.\nCTRL + C will take a while (up to 30s).\n", timeout)
 		}
 	case core.ModeDir:
-		if !validate.ValidateUrl(BaseURL, client, method, random.RandChoice(userAgent)){
+		if !validate.ValidateUrl(BaseURL, client, cfg.Method, random.RandChoice(cfg.UserAgent)){
 	 		os.Exit(1)
 		}
 	}
@@ -245,18 +220,18 @@ func main() {
 	wl := wd.LoadWordlist() // Load Wordlist
 
 	// Basic-Auth
-	if password != "" && username != "" {
-		auth = assemble.BuildBasicAuth(username, password)
+	if cfg.Password != "" && cfg.Username != "" {
+		auth = assemble.BuildBasicAuth(cfg.Username, cfg.Password)
 	}
 
 	
-if !quiet {
+if !cfg.Quiet {
 	fmt.Println("\n\n------------------")
 	fmt.Println("[*] Url: ", BaseURL)
-	fmt.Println("[*] Method: ", method)
-	fmt.Println("[*] Threads: ", threads)
+	fmt.Println("[*] Method: ", cfg.Method)
+	fmt.Println("[*] Threads: ", cfg.Threads)
 	fmt.Println("[*] Timeout: ", timeout)
-	fmt.Println("[*] UAs: ", len(userAgent))
+	fmt.Println("[*] UAs: ", len(cfg.UserAgent))
 	fmt.Print("[*] Mode: ")
 	switch mode{
 	case core.ModeDir:
@@ -271,10 +246,10 @@ if !quiet {
 }
 
 
-	limiter := make(chan struct{}, threads)
+	limiter := make(chan struct{}, cfg.Threads)
 	var dirsChan chan string
 	if mode == core.ModeDir {
-		dirsChan = make(chan string, threads * 50)
+		dirsChan = make(chan string, cfg.Threads * 50)
 	}
 
 	engine := &core.Core{
@@ -283,7 +258,7 @@ if !quiet {
 
 		// Bools
 
-		Recursive:  recursive,
+		Recursive:  cfg.Recursive,
 
 		// Context
 		Ctx:    contextCancel,
@@ -291,19 +266,19 @@ if !quiet {
 		// Config
 		Timeout: timeout,
 		Delay: delay,
-		Quiet: quiet,
+		Quiet: cfg.Quiet,
 
 		// HTTP
 		Client:      client,
-		Method:      method,
-		UserAgents:  userAgent,
+		Method:      cfg.Method,
+		UserAgents:  cfg.UserAgent,
 		AuthHeader:  auth,
-		Header:      header,
-		// Placeholder
-		Placeholder: placeholder,
+		Header:      cfg.Header,
+		// cfg.Placeholder
+		Placeholder: cfg.Placeholder,
 		// Control
-		IgnoreCodes: ignoreCode,
-		Exts:        exts,
+		IgnoreCodes: cfg.IgnoreCode,
+		Exts:        cfg.Exts,
 
 		// Concurrency
 		Limiter:  limiter,
@@ -326,7 +301,7 @@ if !quiet {
 	// Wildcard
 	switch mode {
 	case core.ModeDir:
-		wildcard, err := wildcard.DetectWildcard(client, BaseURL, placeholder, userAgent...)
+		wildcard, err := wildcard.DetectWildcard(client, BaseURL, cfg.Placeholder, cfg.UserAgent...)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(2)
@@ -334,20 +309,20 @@ if !quiet {
 		if wildcard.Active {
 			fmt.Printf("[!] Wildcard detected: %d | %d bytes \n", wildcard.Status, wildcard.Lenght)
 			
-			if method != "GET" && !forceHead{
+			if cfg.Method != "GET" && !cfg.ForceHead{
 				fmt.Printf("[!] Wildcard-like behavior detected using HEAD/SWITCH requests.\n")
 				fmt.Printf("You can skip this confirmation with puting '--force-head'\n")
 				fmt.Printf("HEAD/SWITCH responses do not include a body, so wildcard filtering\ncannot be done reliably and may produce false positives.\n")
-				fmt.Printf("\nSwitch method to 'GET'? [y/N]: \n")
+				fmt.Printf("\nSwitch cfg.Method to 'GET'? [y/N]: \n")
 				
 				if confirmation.WildcardConfirmation() {
-					method = "GET"
+					cfg.Method = "GET"
 				}
 			}
 		}
 		engine.Wildcard = wildcard
 	case core.ModeFuzz:
-		baseline, err := baseline.BuildBaseLine(BaseURL, client, placeholder)
+		baseline, err := baseline.BuildBaseLine(BaseURL, client, cfg.Placeholder)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s", err)
 			os.Exit(1)
